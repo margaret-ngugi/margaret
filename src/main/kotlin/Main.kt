3e9d4e4110fd471e1a99b21f105bed6b749b0c6f
margaret-ngugi <ngugiwairimumargaret@gmail.com>
2023-02-08 17:07:48 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+fun main() {
+    greetMe(name = "Diana")
+}
+fun divide(num1:Int,num2:Int){
+    var result = (num1 % num2)
+    println(result)
+
+}
+
+fun addition(num1:Int,num2:Int){
+    var results = (num1,num2)
+}
+
+
+
+
+fun
+        greetMe(name:String) {
+    println("hello" + name)
+}
+fun modules(num1:Int,num2:Int):Int{
+var modules = num1 % num2
+return(modules)
+}
+fun addition(num1:Int,num2:Int,num3:Int,num4:Int){
+    var sum = num1 + num2 + num3 + num4
+    println(sum)
+}
+fun name (name:String){
+    println("margret,"+name)
+
+}
